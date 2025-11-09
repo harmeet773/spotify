aceed94a0db830bed043ee9d53bd357b5129588b
Harmeet Singh Chugga <harmeetsinghchugga@gmail.com>
2025-11-09 13:00:54 +0530

    commit should work

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
+
+const getRedirectUri = (req) => {
+  const baseUrl = `${req.protocol}://${req.get("host")}`;
+  return `${baseUrl}/callback`;
+};
+
 const port = 3000;
 
 const {
@@ -14,6 +20,8 @@ const {
   REDIRECT_URI,
 } = process.env;
 
-// app.listen(port, () => console.log(`Server running on ${port}`));
+//app.listen(port, () => console.log(`Server running on ${port}`));
 
 app.get('/',(req,res) => { res.json({status:"running"})   }  );
+
+
